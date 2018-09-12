dd2a9eadbcdc0b29e669a906df4a77c86b5ec9a8
SeregaMamonT <mamont@informatics.by>
2018-09-12 23:12:48 +0300

    * Firebase authentication

--- src/components/app-component.vue
+++ src/components/app-component.vue
@@ -2,11 +2,24 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title>{{$t('questions')}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-text-field
+                v-model="email"
+        ></v-text-field>
+        <v-text-field
+                v-model="password"
+                type="password"
+        ></v-text-field>
+        <button @click="signIn">Sign in</button>
+        <button @click="signOut">Sign out</button>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container fluid class="root">
         <add-question-form/>
         <question-list :questions="questions" />
+        <div id="firebaseui-auth-container"></div>
       </v-container>
     </v-content>
   </v-app>
@@ -16,13 +29,16 @@
   import Vue from 'vue';
   import firebase from 'firebase/app';
   import 'firebase/firestore';
+  import 'firebase/auth';
 
   import QuestionList from './question-list.vue';
   import AddQuestionForm from './add-question-form.vue';
 
   let firebaseApp = firebase.initializeApp({
+    authDomain: "messaging-mamo.firebaseapp.com",
     databaseURL: 'https://messaging-mamo.firebaseio.com',
     projectId: 'messaging-mamo',
+    apiKey: "AIzaSyAz-vQfU3Pub_lJg4O9zcvKUcGiLzF2MqY",
   });
 
   const firestore = firebaseApp.firestore();
@@ -34,9 +50,19 @@
       'add-question-form': AddQuestionForm
     },
 
+    created() {
+      firebaseApp.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          console.log(user);
+        }
+      });
+    },
+
     data() {
       return {
         questions: [],
+        email: '',
+        password: '',
       };
     },
 
@@ -45,6 +71,18 @@
         questions: firestore.collection('questions')
       };
     },
+
+    methods: {
+      signIn() {
+        firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+          .then((res) => console.log(res))
+          .catch(console.log);
+      },
+
+      signOut() {
+        firebaseApp.auth().signOut().then();
+      }
+    }
   });
 </script>
 
