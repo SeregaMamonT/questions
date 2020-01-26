a0a70fecf75f40cc673aac79bce652e1d7187227
SeregaMamonT <mamont@informatics.by>
2020-01-26 23:54:48 +0300

    Refactor questionService to async/await

--- src/services/questionService.js
+++ src/services/questionService.js
@@ -12,50 +12,46 @@ export default {
     questionById: 'questionById',
   }),
 
-  add(question) {
-    return checkImageSrc(null, question).then(() => {
-      this.addQuestion(question);
-    });
+  async add(question) {
+    await checkImageSrc(null, question);
+    this.addQuestion(question)
   },
 
   read(id) {
     this.readCurrent(id);
   },
 
-  update(id, question) {
+  async update(id, question) {
     const oldQuestion = this.questionById(question.id);
-    return checkImageSrc(oldQuestion, question).then(() => {
-      return this.updateQuestion({ id, question: question });
-    });
+    await checkImageSrc(oldQuestion, question);
+    return this.updateQuestion({ id, question: question });
   },
 };
 
-function checkImageSrc(oldQuestion, newQuestion) {
+async function checkImageSrc(oldQuestion, newQuestion) {
   const oldImage = oldQuestion && getRazdatkaImage(oldQuestion);
   const newImage = getRazdatkaImage(newQuestion);
 
   if (newImage) {
-    return moveImageToPersistent(newImage).then(newStorageItem => {
-      newQuestion.razdatka.imageSrc = newStorageItem;
-      if (oldImage) {
-        return fileService.deleteFile(oldImage)
-          .catch(err => console.log(err));
+    newQuestion.razdatka.imageSrc = await moveImageToPersistent(newImage);
+    if (oldImage) {
+      try {
+        return await fileService.deleteFile(oldImage);
       }
-    });
+      catch (err) {
+        console.log(err)
+      }
+    }
   }
-  return Promise.resolve();
 }
 
-function moveImageToPersistent(newImage) {
-  if (newImage.domain === 'temp') {
-    const newStorageItem = { domain: 'image', fileName: newImage.fileName };
-    return fileService.copyFile(newImage, newStorageItem)
-      .then(() => {
-        return fileService.deleteFile(newImage)
-          .then(() => newStorageItem);
-      });
+async function moveImageToPersistent(storageItem) {
+  if (storageItem.domain === 'temp') {
+    const newStorageItem = { domain: 'image', fileName: storageItem.fileName };
+    await fileService.copyFile(storageItem, newStorageItem);
+    await fileService.deleteFile(storageItem);
+    return newStorageItem;
   }
-  return Promise.resolve();
 }
 
 function getRazdatkaImage(question) {
